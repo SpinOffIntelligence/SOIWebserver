29f55af70c851ab2cf595853c170b2261b13d91f
SpinOffIntelligence <SpinOffIntelligenceDev@gmail.com>
2016-11-12 09:42:10 -0500

    a

--- app/controllers/soi.js
+++ app/controllers/soi.js
@@ -6,11 +6,20 @@ exports.getOrganizations = function(req, res, next) {
 
 	console.log(odb.server);
 
-	odb.db.query(
-	   'SELECT FROM Organization'
-	).then(function(hitters){
-	   console.log(hitters)
-		res.json(hitters);
-	});
+	// odb.db.query(
+	//    "select Name from (Select expand( out('Worksfor') ) From VPerson Where Name = 'Bob')"
+	// ).then(function(hitters){
+	//    console.log(hitters)
+	// 	res.json(hitters);
+	// });
 
-}
+odb.db.class.get('VOrg').then(function(vorg){
+	vorg.create({
+	   Name:      "VOrgX1",
+	}).then(
+	   function(player){
+	      console.log('Created Record: ' player.name);
+	   }
+	);
+});
+
