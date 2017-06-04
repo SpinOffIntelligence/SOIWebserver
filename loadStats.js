c289860bec0c4d6f3cb1b3074ec5231819e48bec
SpinOffIntelligence <SpinOffIntelligenceDev@gmail.com>
2017-06-04 15:18:23 -0400

    save point

--- loadStats.js
+++ loadStats.js
@@ -7,6 +7,112 @@ var soiControllers = require('./app/controllers/soi');
 var util = require('./components/utilities.js');
 var async = require('async');
 
+function setBetweenScore(infoObj, callback) {
+
+  var query = strUtil.format("UPDATE V SET statsbetweencentrality = %s WHERE @rid = '%s'", infoObj.score, infoObj.id);
+  //console.log('query:' + query);
+  process.stdout.write('+');
+  odb.db.query(query).then(function(records){
+    //console.log('Updated records:' + records);
+    callback(null,records);
+  });
+}
+
+
+function getShortestPath(infoObj, callback) {
+  var objectType = infoObj.objectType;
+
+  var query = strUtil.format("select shortestPath(%s, %s) ", infoObj.src, infoObj.dest);
+  //console.log('query:' + query);
+  process.stdout.write('.');
+  odb.db.query(query).then(function(records){
+    //console.dir(records);
+    var obj = {
+      infoObj: infoObj,
+      data: records
+    }
+    callback(null,obj);
+  });  
+}
+
+function loadStats(infoObj, callback) {
+
+  var count = infoObj.count;
+  
+  mapObjs.push({count: cnt, records: records});
+
+    var allScores = {};
+    //var query = strUtil.format("select @rid from V where @class = '%s'", objectType);
+    var query = strUtil.format("select @rid from V");
+    console.log('query:' + query);
+    odb.db.query(query).then(function(records){
+
+      console.log("records:" + records.length);
+      //console.dir(records);
+
+      var mapObjs = [];
+      var allRecords = records;
+      _.each(allRecords, function(rec) {
+        console.log('source rec:' + rec.rid);
+        var srcId = rec.rid;
+        //_.each(allRecords, function(rec) {
+          dest = allRecords[count];
+          if(dest.rid != srcId) {
+            console.log('>>dest rec:' + dest.rid);
+
+            mapObjs.push({
+              src: srcId,
+              dest: dest.rid
+            })
+
+          }
+        //});
+      });
+
+      console.log('Map Objs:' + mapObjs.length);
+      //console.dir(mapObjs);
+
+      async.map(mapObjs, getShortestPath, function(err, results){
+
+        _.each(results, function(item) {
+
+          // console.log('map result:');
+          // console.dir(item);
+
+          var shortestPath = item.data[0].shortestPath;
+          //console.log('ShortestPath:' + item.infoObj.src + '~' + item.infoObj.dest + ':'  + shortestPath.length);
+          //console.dir(shortestPath);
+          _.each(shortestPath, function(item) {
+            var id = '#' + item.cluster + ':' + item.position;
+            //console.log('id:' + id);
+            if(util.defined(allScores, id)) {
+              var val = allScores[id];
+              allScores[id] = val+1;
+            } else {
+              allScores[id]=1;
+            }
+          });
+
+
+        })
+
+        console.log('Scores:');
+        var mapScoresObjs = [];
+        for (var propertyName in allScores) {
+          score = allScores[propertyName];
+          //console.log(propertyName + ':' + score)
+          mapScoresObjs.push({id: propertyName, score: score});
+        }
+
+        console.log('mapScoresObjs:' + mapScoresObjs.length);
+        //console.dir(mapScoresObjs);
+
+        async.map(mapScoresObjs, setBetweenScore, function(err, results){
+          callback(null, records);
+        });
+      });
+    });
+}
 
 function processStats(statsItem, callback) {
 
@@ -152,11 +258,43 @@ function processStats(statsItem, callback) {
 odb.init(function(err, res) {
   console.log('^^^^')
   console.log('*** statsdegreecentrality');
-  processStats('statsdegreecentrality', function(err, data) {
-      console.log('*** statsdegreecentrality');
-      processStats('statsbetweencentrality', function(err, data) {
-        process.exit(1);
-      });
+  var allRecords;
+
+  var query = strUtil.format("select @rid from V");
+  console.log('query:' + query);
+  odb.db.query(query).then(function(records){
+    var allRecords = records;
+    var mapObjs = [];
+    var cnt=1;
+    _.each(allRecords, fuction(rec) {
+      mapObjs.push({count: cnt, records: records});
+      cnt++;
+    })
+    console.log('loadStats mapObjs:');
+    console.dir(mapObjs);
+
+    async.mapSeries(mapObjs, loadStats, function(err, results){
+      console.log('loadStats mapObjs done!')
+      process.exit(1);            
+    });    
+
+    // loadStats(allRecords, 1, function(err, data) {
+    //     console.log('loadStats done!');
+    //     process.exit(1);
+    // });
+
+
   });
+
+
+
+
+
+  // processStats('statsdegreecentrality', function(err, data) {
+  //     console.log('*** statsdegreecentrality');
+  //     processStats('statsbetweencentrality', function(err, data) {
+  //       process.exit(1);
+  //     });
+  // });
 });
 
